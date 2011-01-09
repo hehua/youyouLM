f51ace0f4647569226b538e8d9c1c090eb9ddf88
unknown <Administrator@.(none)>
2011-01-10 03:00:53 +0800

    add posts

---
+++ SystemIntegration Tests/script���������ļ�/test_link.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<script src="js/prototype.js" type="text/javascript"></script>
+<script src="js/Login.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="css/login.css" />
+<title>TEST SCRIPT</title>
+</head>
+<body >
+
+   <a href="localhost://youyouLM/TestEmploy_list.php">助理管理模块测试</a><br>
+   <a href="localhost://youyouLM/Testnewslist.php">新闻模块测试</a><br>
+   <a href="localhost://youyouLM/TestInformlist.php">通知模块测试</a><br>
+
+</body>
+</html>    
+     
+     
+     
+     
